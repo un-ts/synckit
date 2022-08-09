4bcf636e1d23851f3f11b6a1abf9a29da50a30a2
Noah <noahnu@gmail.com>
2022-08-08 21:15:33 -0400

    cr: update src/index.ts
    Co-authored-by: JounQin <admin@1stg.me>

--- src/index.ts
+++ src/index.ts
@@ -217,7 +217,7 @@ const setupTsRunner = (
         !NODE_OPTIONS?.includes(resolvedPnp) &&
         !execArgv.includes(resolvedPnp)
       ) {
-        execArgv = [...execArgv, `-r ${resolvedPnp}`]
+        execArgv.push(`-r ${resolvedPnp}`)
       }
     } catch {}
   }
