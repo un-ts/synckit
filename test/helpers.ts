5cedc7b92679543c15e9069049cd3c5ad27095ac
jakub.jedlikowski <jakub.jedlikowski@allegro.com>
2024-10-02 14:33:03 +0200

    chore: remove unnecessary __esModule flag in mock

--- test/helpers.ts
+++ test/helpers.ts
@@ -26,7 +26,6 @@ export const setupReceiveMessageOnPortMock =
       return {
         ...WorkerThreads,
         receiveMessageOnPort: jest.fn(WorkerThreads.receiveMessageOnPort),
-        __esModule: true,
       }
     })
 
