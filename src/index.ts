246809df1b06254b53dbf0e11619d6879329505d
Nikolas Schröter <25958801+nwidynski@users.noreply.github.com>
2025-03-19 17:50:21 +0100

    feat: add node version safeguard

--- src/index.ts
+++ src/index.ts
@@ -231,6 +231,9 @@ const setupTsRunner = (
 
     switch (tsRunner) {
       case TsRunner.Node: {
+        if (NODE_VERSION < STRIP_TYPES_SUPPORTED_NODE_VERSION) {
+          throw new Error(`type stripping is not supported in this node version`)
+        }
         execArgv = [
           STRIP_TYPES_FLAG,
           ...execArgv.filter(arg => arg !== NO_STRIP_TYPES_FLAG),
