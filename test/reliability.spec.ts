dd662757d08ab1aee7a929171b8a182965e63a50
fisker Cheung <lionkay@gmail.com>
2025-03-25 00:44:30 +0800

    chore: increase

--- test/reliability.spec.ts
+++ test/reliability.spec.ts
@@ -5,7 +5,7 @@ import { _dirname } from './helpers.js'
 const workerPath = path.resolve(_dirname, './worker-async-identity.js')
 
 // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-const times = process.env.CI ? 1e4 : 1e3
+const times = process.env.CI ? 1e5 : 1e3
 
 test(`Reliability (${times} runs)`, async () => {
   const { createSyncFn } = await import('synckit')
