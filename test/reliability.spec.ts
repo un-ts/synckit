82064fe7dd416b8fb53d1d0caf523da28ae1fd0f
fisker <lionkay@gmail.com>
2025-04-09 23:49:56 +0800

    More workers

--- test/reliability.spec.ts
+++ test/reliability.spec.ts
@@ -2,16 +2,21 @@ import path from 'node:path'
 
 import { _dirname } from './helpers.js'
 
-const workerPath = path.resolve(_dirname, './worker-async-identity.js')
-
 // eslint-disable-next-line @typescript-eslint/no-magic-numbers
 const times = process.env.CI ? 1e6 : 1e3
 
 test(`Reliability (${times} runs)`, async () => {
   const { createSyncFn } = await import('synckit')
-  const identity = createSyncFn(workerPath)
+  const functions = Array.from({ length: 3 }, (_, index) =>
+    createSyncFn(path.resolve(_dirname, `./worker-identity-${index + 1}.js`)),
+  )
 
   for (let index = 0; index < times; index++) {
-    expect(identity(index)).toBe(index)
+    try {
+      expect(functions[index % functions.length](index)).toBe(index)
+    } catch (error) {
+      console.log(`Failed on ${index + 1} run.`)
+      throw error
+    }
   }
 })
