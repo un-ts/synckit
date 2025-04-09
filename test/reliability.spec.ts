83e1eec458039295f7a8f967f04eda68452c9c37
fisker <lionkay@gmail.com>
2025-04-09 23:56:38 +0800

    Back to 1 worker

--- test/reliability.spec.ts
+++ test/reliability.spec.ts
@@ -3,17 +3,15 @@ import path from 'node:path'
 import { _dirname } from './helpers.js'
 
 // eslint-disable-next-line @typescript-eslint/no-magic-numbers
-const times = process.env.CI ? 1e6 : 1e3
+const times = process.env.CI ? 1e5 : 1e3
 
 test(`Reliability (${times} runs)`, async () => {
   const { createSyncFn } = await import('synckit')
-  const functions = Array.from({ length: 3 }, (_, index) =>
-    createSyncFn(path.resolve(_dirname, `./worker-identity-${index + 1}.js`)),
-  )
+  const identity = createSyncFn(path.resolve(_dirname, `./worker-identity.js`))
 
   for (let index = 0; index < times; index++) {
     try {
-      expect(functions[index % functions.length](index)).toBe(index)
+      expect(identity(index)).toBe(index)
     } catch (error) {
       console.log(`Failed on ${index + 1} run.`)
       throw error
